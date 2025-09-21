1b1e949babe06efcee408bf7e9aeb3bc17278421
balancetheworld <2539888062@qq.com>
2025-09-21 22:59:46 +0800

    第三次提交，aboutme主页开发完成

--- react/src/pages/AboutMe/AboutMe.tsx
+++ react/src/pages/AboutMe/AboutMe.tsx
@@ -3,15 +3,21 @@ import Avatar from "./avatar/avater";
 import SayHello from './sayHello/sayHello';
 import CountPart from './CountPart/CountPart'
 import BottomBox from './BottomPart/BottomPart'
+import NavPart from './navPart/navPart'
 import './AboutMe.scss'
 const AboutMe=()=>{
 const isDark=true;
     return(
         <div className={isDark?"aboutmeBox-Dark":"aboutmeBox-Light"}>
-         <Avatar isDark={isDark}/>
+              <NavPart isDark={isDark}/>
+            <div>
+        <Avatar isDark={isDark}/>
         <SayHello isDark={isDark}/>
         <CountPart isDark={isDark}/>
         <BottomBox isDark={isDark}/>
+      
+            </div>
+       
         </div>
       
     )}
---
+++ react/src/pages/AboutMe/navPart/navPart.tsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+import "./navPart.scss"
+import "./navPartIcon/iconfont.css"
+
+interface AvatarProps{
+  isDark:boolean;
+}
+const NavPart:React.FC<AvatarProps> = ({isDark}) => {
+const [activeLink,setActiveLink]=useState("/aboutme");
+
+const ButtonList=[
+  {icon:"iconfont icon-home1-copy",text:"Home",Link:"/"},
+   {icon:"iconfont icon-ic_yonghu1",text:"Me",Link:"/aboutme"},
+  {icon:"iconfont icon-jishufuwu",text:"Experience",Link:"//"},
+  {icon:"iconfont icon-xiaolian",text:"Interest",Link:"///"},
+ 
+]
+
+
+
+const handleItemClick=(Link:string)=>{
+  setActiveLink(Link);
+}
+
+  return(
+<div className="navPart">
+ <div className={isDark?"navPart-Dark":"navPart-Light"}>
+      <ul>
+      {ButtonList.map((item,index)=>(
+        <li key={index}
+        className={activeLink===item.Link? "active" : ""}
+        onClick={()=>handleItemClick(item.Link)}>
+          <Link to={item.Link}>
+          <span className='icon'>
+               <i className={item.icon}></i>
+          </span>
+            <span className="text">{item.text}</span>
+          </Link>
+        </li>
+      ))}
+      </ul>
+    </div>
+</div>
+    )
+}
+export default NavPart;
